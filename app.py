cdc65102944738998491c7409039246828de7ba5
YMTSTK <87373329+YMTSTK@users.noreply.github.com>
2023-05-06 16:19:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,jsonify
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
+import pickle
+
+
+model = pickle.load(open('sms_classification.sav','rb'))
+app = Flask(__name__)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+  sm=request.form.get('sms')
+
+  list = [sm]
+  from sklearn.preprocessing import LabelEncoder
+  le = LabelEncoder()
+  li = le.fit_transform(list)
+
+  result = model.predict([li])[0]
+  return jsonify({'error': 'false', 'data': str(result), 'message': 'Success'})
+
+if __name__=='main':
+    app.run(debug=True)
